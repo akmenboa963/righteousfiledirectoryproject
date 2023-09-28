68195093f8770d93259f72472228909bb9a8ac75
akmenboa963 <144290042+akmenboa963@users.noreply.github.com>
2023-09-28 11:04:26 +0100

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,26 @@
+# Hi, I'm Andrew Osei 👋
+
+I'm passionate about creating solutions through tech to provide a positive impact on the world. I'm currently learning cloud environments starting with AWS.
+
+Connect with me here:
+[![Linkedin Badge](https://img.shields.io/badge/-Andrew%20Osei-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/andrew-osei-04b24959/)](https://www.linkedin.com/in/andrew-osei-04b24959/)
+[![Medium Badge](https://img.shields.io/badge/Andrew%20Osei-12100E?style=flat-square&logo=medium&logoColor=white&link=https://medium.com/@andrewx-ex)](https://medium.com/@andrewx-ex)
+[![Gmail Badge](https://img.shields.io/badge/-andy%40x--ex.co-c14438?style=flat-square&logo=Gmail&logoColor=white)](mailto:andy@x-ex.co)
+
+## ⚡ Technologies
+
+![Amazon AWS](https://img.shields.io/badge/Amazon%20AWS-232F3E?style=flat-square&logo=amazon-aws)
+![Git](https://img.shields.io/badge/-Git-black?style=flat-square&logo=git)
+![GitHub](https://img.shields.io/badge/-GitHub-181717?style=flat-square&logo=github)
+![Python](https://img.shields.io/badge/-Python-black?style=flat-square&logo=Python)
+![Linux](https://img.shields.io/badge/Linux-FCC624?style=flat-square&logo=linux&logoColor=black)
+![Trello](https://img.shields.io/badge/Trello-%23026AA7.svg?style=flat-square&logo=Trello&logoColor=white)
+![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white)
+![Terraform](https://img.shields.io/badge/terraform-%235835CC.svg?style=for-the-badge&logo=terraform&logoColor=white)
+
+## Github Stats
+
+![Github Stats](https://github-readme-stats.vercel.app/api?username=akmenboa963&count_private=true&show_icons=true&include_all_commits=true)
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=akmenboa963&hide=TeX&layout=compact)
+
+[![Visitors](https://api.visitorbadge.io/api/visitors?path=akmenboa963%2Fakmenboa963&label=VISITORS&countColor=%23263759)](https://visitorbadge.io/status?path=akmenboa963%2Fakmenboa963)
